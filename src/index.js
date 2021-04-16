083e0a2477147d40dd88dd4ad7e82172efbe7c5b
Diego Rufino <diegorufino1@gmail.com>
2021-04-15 22:26:02 -0300

    Add o heroku

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 import 'bootswatch/dist/minty/bootstrap.min.css'
-import './index.css';
-import App from './App';
+import './index.css'
+import App from './App'
+
+import axios from 'axios'
+
+axios.get('http://localhost:3000/apostas').then(response => {
+    console.log(response.data)
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/views/home.js
+++ src/views/home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 class Home extends React.Component {
+
     render() {
         return(
             <div className="card">
@@ -8,7 +9,50 @@ class Home extends React.Component {
                     Aposta
                 </div>
                 <div className="card-body">
-                    
+                    <div className="row">
+                    <div className="col-md-4">
+                            <div className="form-group">
+                                <label>Nome: *</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    defaultValue={''}
+                                />
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <label>Telefone: *</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    defaultValue={''}
+                                />
+                            </div>
+                        </div>
+                        <div className="col-md-2">
+                            <div className="form-group">
+                                <label>Número: *</label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    defaultValue={0}
+                                />
+                            </div>
+                        </div>
+                        <div className="col-md-1">
+                            <div className="form-group">
+                                <label>&nbsp;</label>
+                                <button type="button" className="btn btn-primary">Enviar</button>
+                            </div>
+                        </div>
+                        <div className="col-md-1">
+                            <div className="form-group">
+                                <label>&nbsp;</label>
+                                <button type="button" className="btn btn-secondary">Limpar</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
