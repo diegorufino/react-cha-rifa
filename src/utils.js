8c6d8661bf5b54739889cecfd29c05b889d4b9bf
Diego Rufino <diegorufino1@gmail.com>
2021-05-04 22:30:46 -0300

    Lista de Apostas

--- src/utils.js
+++ src/utils.js
@@ -6,5 +6,19 @@ export function pad_with_zeroes(number, length) {
     }
 
     return my_string;
-
 }
+
+export const maskPhone = value => {
+    return value
+      .replace(/\D/g, "")
+      .replace(/(\d{2})(\d)/, "($1) $2")
+      .replace(/(\d{5})(\d{4})(\d)/, "$1-$2");
+};
+  
+export const maskDate = value => {
+    return value
+      .replace(/\D/g, "")
+      .replace(/(\d{2})(\d)/, "$1/$2")
+      .replace(/(\d{2})(\d)/, "$1/$2")
+      .replace(/(\d{4})(\d)/, "$1");
+  };
--- src/views/admin/apostas.js
+++ src/views/admin/apostas.js
@@ -1,18 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Card from '../../components/card'
+import { maskPhone } from '../../utils'
 
-const ConsultaApostas = () => {
+const API_URL = 'http://rest-api-cha-rifa.herokuapp.com/apostas'
+
+const ConsultaApostas = props => {
 
     const [apostas, setApostas] = useState([])
 
     const listarApostas = () => {
-        axios.get('http://rest-api-cha-rifa.herokuapp.com/apostas')
+        axios.get(API_URL)
             .then(response => {
                 const listaDeApostas = response.data
                 setApostas(listaDeApostas.response.apostas)
             }).catch(erro => {
                 console.log(erro)
+        })
+    }
+
+    const alterarStatus = (id) => {
+        axios.patch(`${API_URL}/${id}`, null, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            listarApostas()
+        }).catch(erro => {
+            console.log(erro)
         })
     }
 
@@ -39,14 +54,20 @@ const ConsultaApostas = () => {
                     {apostas.map(aposta => {
                         return (
                             <tr>
-                                <th>{aposta.id_aposta}</th>
-                                <th>{aposta.codigo}</th>
-                                <th>{aposta.numero}</th>
-                                <th>{aposta.id_sorteio}</th>
-                                <th>{aposta.data_criado}</th>
-                                <th>{aposta.nome}</th>
-                                <th>{aposta.celular}</th>
-                                <th>{aposta.data_pagamento}</th>
+                                <td>{aposta.id_aposta}</td>
+                                <td>{aposta.codigo}</td>
+                                <td>{aposta.numero}</td>
+                                <td>{aposta.id_sorteio}</td>
+                                <td>{aposta.data_criado}</td>
+                                <td>{aposta.nome}</td>
+                                <td>{maskPhone(aposta.celular)}</td>
+                                <td>
+                                    {
+                                        aposta.data_pagamento ?
+                                            aposta.data_pagamento :
+                                            <button onClick={e => alterarStatus(aposta.id_aposta)} type="button" className="btn btn-success">Add Pag</button>
+                                    }
+                                </td>
                             </tr>
                         )
                     }
